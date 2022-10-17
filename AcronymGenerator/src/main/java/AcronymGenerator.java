3935c3242c037b5c516500db947b87b8d7a9ec7b
Andreas <22032770+AEkman@users.noreply.github.com>
2022-10-17 11:30:56 +0200

    solution 1

--- AcronymGenerator/src/main/java/AcronymGenerator.java
+++ AcronymGenerator/src/main/java/AcronymGenerator.java
@@ -1,2 +1,32 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class AcronymGenerator {
+    private static String getFirstLetterOfWord(String word) {
+        if (word.equals("von")) {
+            return "v";
+        } else {
+            return String.valueOf(word.charAt(0))
+                         .toUpperCase();
+        }
+    }
+
+    private static String[] splitWordBySpaceOrDash(String input) {
+        return input.replace("-", " ")
+                    .split(" ");
+    }
+
+    public String generate(String input) {
+        if (input.isEmpty()) {
+            return "";
+        }
+
+        String[] words = splitWordBySpaceOrDash(input);
+
+        return Arrays.stream(words)
+                     .map(AcronymGenerator::getFirstLetterOfWord)
+                     .collect(Collectors.joining());
+
+    }
+
 }
--- AcronymGenerator/src/test/java/AcronymGeneratorTest.java
+++ AcronymGenerator/src/test/java/AcronymGeneratorTest.java
@@ -1,13 +1,26 @@
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class AcronymGeneratorTest {
 
+    private static final AcronymGenerator acronymGenerator = new AcronymGenerator();
+
     @ParameterizedTest
-    @CsvSource()
+    @CsvSource({
+            "'',''",
+            "Thomas Meyer, TM",
+            "martin schmidt, MS",
+            "Jan Erich Mueller, JEM",
+            "Jan-Erich Schmidt, JES",
+            "Paul Meyer-Schmidt, PMS",
+            "Paul von Lahnstein, PvL",
+            "Martin von Lahnstein-Meyer, MvLM"
+    })
     void testSomething(String input, String expected) {
-        fail("Start here!");
+        var result = acronymGenerator.generate(input);
+
+        assertThat(result).isEqualTo(expected);
     }
 }
